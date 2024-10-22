ec5296cd60b954cceccdd9ddb434273e9939539a
sveitser <sveitser@gmail.com>
2024-10-22 11:42:38 +0200

    flake: use clang as CC

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
@@ -20,26 +20,41 @@
         };
       in
       {
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            stableToolchain
-            bashInteractive
-            coreutils
-            python3
+        devShells.default = let llvmPkgs = pkgs.llvmPackages_15; in llvmPkgs.stdenv.mkDerivation
+          {
+            name = "clang-dev-shell";
+            buildInputs = with pkgs; [
+              openssl
+              pkg-config
+              nixpkgs-fmt
 
-            rust-cbindgen
-            wabt
-          ] ++ lib.optionals stdenv.isDarwin
-            [ darwin.apple_sdk.frameworks.SystemConfiguration ];
+              stableToolchain
+              llvm
 
-          shellHook = ''
-            # Using a local cargo home to avoid polluting the user's cargo home
-            export CARGO_HOME=$PWD/.cargo
-            export PATH=$CARGO_HOME/bin:$PATH
-          '';
-        };
+              bashInteractive
+              coreutils
+              git
+              python3
+
+              rust-cbindgen
+              wabt
+              python3
+            ] ++ lib.optionals stdenv.isDarwin
+              [ darwin.apple_sdk.frameworks.SystemConfiguration ];
+
+            shellHook = ''
+              export C_INCLUDE_PATH="${llvmPkgs.libclang.lib}/lib/clang/${llvmPkgs.libclang.version}/include"
+              export LIBCLANG_PATH=
+              export CC="${llvmPkgs.clang}/bin/clang"
+              export CXX="${llvmPkgs.clang}/bin/clang++"
+              export AR="${llvmPkgs.llvm}/bin/llvm-ar"
+              export CFLAGS="-mcpu=generic"
+
+              # Using a local cargo home to avoid polluting the user's cargo home
+              export CARGO_HOME=$PWD/.cargo
+              export PATH=$CARGO_HOME/bin:$PATH
+            '';
+          };
       }
     );
 }
